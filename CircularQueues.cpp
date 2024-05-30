a44ba38b27f97968aa984e4e3ad1edc658fc60ed
nabilasaf <nabilasafitri861@gmail.com>
2024-05-30 15:50:32 +0700

    menapilkan

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -40,7 +40,7 @@ public:
         queue_array[REAR] = num;
     }
 
-    void remover() {
+    void remove() {
         //cek apakah antrian kosong
         if (FRONT == -1) {
             cout << "Queue underflow\n";
@@ -99,3 +99,55 @@ public:
     }
 };
 
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display Values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4)" << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+                case '1':
+                {
+                    q.insert();
+                    break;
+                }
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
+                case '4':
+                {
+                    return 0;
+                }
+                default: {
+                    cout << "Invalid option!!" << endl;
+                    break;
+                }
+            }
+        }
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+    return 0;
+}
+};
