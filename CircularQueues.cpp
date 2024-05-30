822307bbcfddf0393b46d9fa9aac8fbe349fcca5
nabilasaf <nabilasafitri861@gmail.com>
2024-05-30 15:40:52 +0700

    iterasi dari awal array hingga REAR

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -90,4 +90,12 @@ public:
             }
             FRONT_Position = 0;
 
-            
+            //Iterasi dari awal array hingga REAR
+            while (FRONT_Position <= REAR_Position) {
+                cout << queue_array[FRONT_Position] << " ";
+            FRONT_Position;
+        }
+        cout << endl;
+    }
+};
+
