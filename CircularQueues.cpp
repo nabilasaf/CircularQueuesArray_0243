71d87d28c612c82a56ae292d87fd3189cab12ed1
nabilasaf <nabilasafitri861@gmail.com>
2024-05-30 15:18:50 +0700

    jika FRONT <= REAR, iterasi dari FRONT hingga REAR

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -72,6 +72,13 @@ public:
             return;
         }
 
-        
+        cout << "\nElements in the Queue are...\n";
+
+        //jika FRONT <= REAR, iterasi dari FRONT hingga REAR
+        if (FRONT_Position <= REAR_Position) {
+            while (FRONT_Position <= REAR_Position) {
+                cout << queue_array[FRONT_Position] << " ";
+            }
+        }
     }
 };
