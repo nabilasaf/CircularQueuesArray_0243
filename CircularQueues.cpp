1a6ac512f24753d983981b9352af29bb4b921366
nabilasaf <nabilasafitri861@gmail.com>
2024-05-30 14:58:39 +0700

    memperbaiki error

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class Queue {
+class Queues{
 private:
     static const int max = 3;
     int FRONT, REAR;
     int queue_array[max];
 
 public:
-    Queues() {
+    Queues() 
+    {
         FRONT = -1;
         REAR = -1;
     }
@@ -18,26 +19,24 @@ public:
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
-    }
-
-    //cek apakah antrian penuh
-    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-        cout << "\nQueue overflow\n";
-        return;
-    }
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
 
         //cek apakah antrian kosong
-    if (FRONT == -1) {
-        FRONT = 0;
-        REAR = 0;
-    }
-    else {
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
         //jika REAR berada di posisi terakhir array, kembali ke awal array
         if (REAR == max - 1)
             REAR = 0;
         else 
             REAR = REAR + 1;
-    }
-    queue_array[REAR] = num;
+        }
+        queue_array[REAR] = num;
     }
 };
